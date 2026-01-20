775fbd0027e72a7c1f35884bb4a036fab0bf41e7
wlandau <will.landau.oss@gmail.com>
2026-01-20 15:38:46 -0500

    condense vcov()

--- R/vcov.R
+++ R/vcov.R
@@ -1,46 +1,11 @@
-#' @title Nonlinear fixed effects covariance matrix: proportional
-#'   decline model
+#' @title Treatment effect parameter covariance matrix
 #' @export
 #' @family estimates and predictions
-#' @description Extract the covariance matrix of the proportional
-#'   decline model.
+#' @description Extract the covariance matrix of the treatment effect
+#'   parameters of a progression model for repeated measures.
 #' @return A matrix with covariance of each pair of `theta` parameters.
-#'   Rows and columns are labeled by study arm.
-#' @inheritParams summary.pmrm_fit
-#' @examples
-#'   set.seed(0L)
-#'   simulation <- pmrm_simulate_decline(
-#'     visit_times = seq_len(5L) - 1,
-#'     gamma = c(1, 2)
-#'   )
-#'   fit <- pmrm_model_decline(
-#'     data = simulation,
-#'     outcome = "y",
-#'     time = "t",
-#'     patient = "patient",
-#'     visit = "visit",
-#'     arm = "arm",
-#'     covariates = ~ w_1 + w_2
-#'   )
-#'   vcov(fit)
-vcov.pmrm_fit_decline <- function(object, ...) {
-  full <- RTMB::sdreport(object$model, getReportCovariance = TRUE)$cov.fixed
-  theta <- full[rownames(full) == "theta", colnames(full) == "theta"]
-  labels <- pmrm_data_labels(object$data)
-  arms <- levels(object$data[[labels$arm]])[-1L]
-  rownames(theta) <- arms
-  colnames(theta) <- arms
-  theta
-}
-
-#' @title Nonlinear fixed effects covariance matrix: non-proportional
-#'   slowing model
-#' @export
-#' @family estimates and predictions
-#' @description Extract the covariance matrix of the non-proportional
-#'   slowing model.
-#' @return A matrix with covariance of each pair of `theta` parameters.
-#'   Rows and columns are labeled by study arm and scheduled visit.
+#'   Rows and columns are labeled (by just study arm for proportional models,
+#'   arm and visit for non-proportional models.)
 #' @inheritParams summary.pmrm_fit
 #' @examples
 #'   set.seed(0L)
@@ -58,11 +23,16 @@ vcov.pmrm_fit_decline <- function(object, ...) {
 #'     covariates = ~ w_1 + w_2
 #'   )
 #'   vcov(fit)
-vcov.pmrm_fit_slowing <- function(object, ...) {
+vcov.pmrm_fit <- function(object, ...) {
   full <- RTMB::sdreport(object$model, getReportCovariance = TRUE)$cov.fixed
   theta <- full[rownames(full) == "theta", colnames(full) == "theta"]
   labels <- pmrm_data_labels(object$data)
   arms <- levels(object$data[[labels$arm]])[-1L]
+  if (object$constants$proportional) {
+    rownames(theta) <- arms
+    colnames(theta) <- arms
+    return(theta)
+  }
   visits <- levels(object$data[[labels$visit]])[-1L]
   original_arms <- rep(arms, times = object$constants$J - 1L)
   original_visits <- rep(visits, each = object$constants$K - 1L)
